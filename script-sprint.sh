ab6b876b70315cf08c8e87d17225e383a0a43edd
MarcosHVB <marcos.boas@sptech.school>
2023-05-30 10:54:39 -0300

    mais ajustes script-sprint

--- script-sprint.sh
+++ script-sprint.sh
@@ -2,20 +2,19 @@
 
 sudo apt-get update && sudo apt-get upgrade -y #atualiza o ambiente
 
-cd ..
+cd /root/Desktop/
 sleep 2
 
 diretorio="/root/Desktop/Jar-Docker"
 
 if [ -d "$diretorio" ]; #se retorno for igual a 0
 	then #entao,
-		git clone https://github.com/didiegovieira/Jar-Docker.git
-		cd Jar-Docker/jar-individual/target
-		java -jar jar-individual-1.0-SNAPSHOT-jar-with-dependencies.jar
-
-	else #se nao,root
 		git fetch && git pull
 		cd jar-individual/jar-individual/target
+		java -jar jar-individual-1.0-SNAPSHOT-jar-with-dependencies.jar
+	else #se nao,root
+		git clone https://github.com/didiegovieira/Jar-Docker.git
+		cd Jar-Docker/jar-individual/target
 		java -jar jar-individual-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 fi #fecha o 1o if
