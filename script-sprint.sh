7c5b950273355166d301c93c5de40776f20a5ef6
MarcosHVB <marcos.boas@sptech.school>
2023-05-30 11:07:42 -0300

    acessando jar-docker quando ele ja estiver criado

--- script-sprint.sh
+++ script-sprint.sh
@@ -6,16 +6,14 @@ cd ..
 sleep 2
 
 diretorio="Jar-Docker"
-echo -d "$diretorio veriricando se existe"
+
 if [ -d "$diretorio" ]; #se retorno for igual a 0
 	then #entao,
-		echo "travei no then"
-		
+		cd Jar-Docker
 		git pull
 		cd Jar-Docker/jar-individual/target
 		java -jar jar-individual-1.0-SNAPSHOT-jar-with-dependencies.jar
 	else #se nao,root
-		echo "travei no else"
 		git clone https://github.com/didiegovieira/Jar-Docker.git
 		cd Jar-Docker/jar-individual/target
 		java -jar jar-individual-1.0-SNAPSHOT-jar-with-dependencies.jar
